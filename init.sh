f525d39feb17b9c97a25b85e2860c55a8aa1fcd5
Jimmy Yang <gankkank@gmail.com>
2015-07-17 01:24:17 +0800

    disable bash_completion for mac

--- init.sh
+++ init.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 script_path=${BASH_SOURCE[0]}
 script_dir=$(dirname $script_path)
-cd ${script_dir}/
+#cd ${script_dir}/
+
+enable_completion=0
+
+function enable_mac_completion() {
+  [ -d /usr/local/etc/ ] || mkdir /usr/local/etc/
+  [ -f /usr/local/etc/bash_completion ] || cp bash_completion /usr/local/etc/bash_completion
+  . /usr/local/etc/bash_completion
+}
 
 if [ "$(uname)" == "Darwin" ]; then
-  . ps1.mac.sh
-  . bash_completion
+  . ${script_dir}/ps1.mac.sh
+  [ "$enable_completion" == "0" ] || enable_mac_completion
 else
-  . ps1.sh
+  . ${script_dir}/ps1.sh
 fi
 
-#[ "$(uname)" == "Darwin" ] && . ps1.mac.sh || . ps1.sh
 
-. alias.sh
-. ssh-agent.sh
+. ${script_dir}/alias.sh
+. ${script_dir}/ssh-agent.sh
 
 load_ssh_keys_from ~/.ssh-agent.keys
